ea6b90e52fc54386d534f9b6db6a8c3260585f25
Faust-jiang <feng.jiang@uzh.ch>
2025-07-31 11:14:02 +0200

    Delete sigformer/sigformer directory

--- sigformer/sigformer/hedger/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import BaseHedger
-from .deep_hedger import DeepHedger
-from .delta_hedger import HestonDeltaHedger, RBergomiDeltaHedger
-from .sig_hedger import SigHedger, SigHedger_v2, SignatureOnlyHedger, TransformerHedger
--- sigformer/sigformer/nn/utils.py
+++
@@ -1,5 +0,0 @@
-import jax.random as jrandom
-
-
-def split_key(key):
-    return None if key is None else jrandom.split(key, 1)[0]
